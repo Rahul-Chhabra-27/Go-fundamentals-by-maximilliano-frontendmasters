1a02871f6b1ceddef1b18d5fe79415c501256a95
Rahul-Chhabra-27 <chhabrarahul2707@gmail.com>
2024-03-27 17:48:07 +0530

    Packages

--- functions.go
+++ functions.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	f "fmt"
-	add "rahulchhabra.io/calc/adder";
-	mul "rahulchhabra.io/calc/multiplier"
+	"rahulchhabra.io/calc"
 )
 
 func greet() {
 	f.Println("Hello World!")
 	f.Println(url)
-	f.Println(add.Add(1, 2))
-	f.Println(mul.Mul(2, 3))
+	f.Println(calc.Add(1, 2))
+	f.Println(calc.Mul(2, 3))
 }
